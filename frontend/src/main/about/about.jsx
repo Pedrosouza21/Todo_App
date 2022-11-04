53269414abdc6771be52fd6ffc6f5f2bf27a83d8
pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-11-04 11:46:01 -0300

    140 - PageHeader Component

--- frontend/src/main/about/about.jsx
+++ frontend/src/main/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div> 
-        <h1> Sobre</h1>
+        <PageHeader name='Sobre' small='Nos'></PageHeader>
+        
+        <h2>Nossa Historia</h2>
+        <p> lorem ispum dolor sit amet..</p>
+        <h2> Missao e Visao</h2>
+        <p> lorem ispum dolor</p>
+        <h2> Imprensa</h2>
+        <p> oiiii</p>
     </div>
 )
